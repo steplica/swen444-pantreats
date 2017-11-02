1e39220b0a2397b730db231287559babe6b3e5b4
Robert M. Nill <rmnill202mail@gmail.com>
2017-11-02 10:08:07 -0400

    Outlined basic HTML divs, applied CSS grids with temporary colors

--- src/components/RecipePage.vue
+++ src/components/RecipePage.vue
@@ -49,5 +49,57 @@
 
 <!-- CSS; scoped keyword added to restrict CSS to this component -->
 <style scoped>
+    #recipePage { /* Grid */
+        display: grid;
+        grid-template-rows: repeat(3, minmax(50px ,auto));
+        grid-template-columns: repeat(5, minmax(50px, auto));
+        grid-gap: 5px;
+        background-color: #ffffff;
+    }
+    #recipeSummary {
+        grid-row: 1 / 2;
+        grid-column: 1 / 4;
 
+        outline-color: blueviolet;
+        outline-width: 2px;
+        outline-style: solid;
+    }
+    #recipeImages {
+        grid-row: 1 / 2;
+        grid-column: 4 / 6;
+
+        outline-color: #32e24b;
+        outline-width: 2px;
+        outline-style: solid;
+    }
+    #recipeDetails {
+        grid-row: 2 / 3;
+        grid-column: 1 / 6;
+
+        outline-color: #e27219;
+        outline-width: 2px;
+        outline-style: solid;
+    }
+    #recipeIngredients {
+        grid-row: 3 / 4;
+        grid-column: 1 / 2;
+
+        outline-color: #3db8e2;
+        outline-width: 2px;
+        outline-style: solid;
+    }
+    #recipeCookware {
+
+        outline-color: #d3e23b;
+        outline-width: 2px;
+        outline-style: solid;
+    }
+    #recipeSteps {
+        grid-row: 3 / 5;
+        grid-column: 2 / 6;
+
+        outline-color: #32e233;
+        outline-width: 2px;
+        outline-style: solid;
+    }
 </style>
