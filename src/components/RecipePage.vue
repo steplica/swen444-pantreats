ba84d11185f33537eb2ae0db0e698b32cb9af01f
Robert M. Nill <rmnill202mail@gmail.com>
2017-11-04 09:18:46 -0400

    Initial layout of mock data. Planning to pull real data from the database and use that now.

--- src/components/RecipePage.vue
+++ src/components/RecipePage.vue
@@ -15,65 +15,118 @@
     <div id="recipePage">
         <!-- Summary -->
         <div id="recipeSummary">
-            <md-layout md-align="center">
-                <h5>{{name}}</h5>
-            </md-layout>
-
-            <!--
-             Score -->
-            <!--
-             Reviews | made it | photos
-             Recipe by: Username -->
-             {{description}}
+            <div id="recipeSummaryTitle">
+                <span class="md-display-1">{{name}}</span>
+                <md-rating-bar style="margin:auto" v-model="rating" :md-max-rating="5" class="md-primary" disabled></md-rating-bar>
+                <!-- Reviews | made it | photos -->
+                <md-button class="md-primary">{{reviews.length}} Reviews</md-button>
+                <md-button class="md-primary">{{madeIt}} Made It</md-button>
+                <md-button class="md-primary">{{photos.length}} Photos</md-button>
+            </div>
+            <span class="md-body-2">Recipe by <u>{{submitter}}</u></span><br>
+            <span class="md-subheading">{{description}}</span><br>
+            <div id="summaryFlexButtons">
+                <md-button class="summaryButtons md-raised md-accent">Save</md-button>
+                <md-button class="summaryButtons md-raised md-accent">Made It</md-button>
+                <md-button class="summaryButtons md-raised md-accent">Review</md-button>
+                <md-button class="summaryButtons md-raised md-accent">Report</md-button>
+            </div>
         </div>
         <!-- Images -->
         <div id="recipeImages">
-            Images Placeholder
+            <!-- Primary image -->
+            <md-image class="recipePrimaryImage" :md-src="photos[0]" alt="Recipe Image"></md-image>
+
+            <!-- Show smaller previews for non-primary images -->
+            <div id="recipeSecondaryImages">
+                <md-image v-for="photo, key, index in photos" v-if="index < 3" class="recipeSecondaryImage" :md-src="photo" alt="Recipe"></md-image>
+                <md-button id="recipeImageGallery" class="md-raised md-accent" v-else-if="index == 4">+</md-button>
+            </div>
         </div>
         <!-- Details -->
         <div id="recipeDetails">
-            Details Placeholder
+            <div class="detailsFlexItem">
+                <span class="md-subheading"><b>Prep Time</b></span><br>
+                <span class="md-body-1">{{prepTime}} Minutes</span>
+            </div>
+            <div class="detailsFlexItem">
+                <span class="md-subheading"><b>Cook Time</b></span><br>
+                <span class="md-body-1">{{cookTime}} Minutes</span>
+            </div>
+            <div class="detailsFlexItem">
+                <span class="md-subheading"><b>Total Time</b></span><br>
+                <span class="md-body-1">{{prepTime + cookTime}} Minutes</span>
+            </div>
+            <div class="detailsFlexItem">
+                <span class="md-subheading"><b>Serves</b></span><br>
+                <span class="md-body-1">{{servings}} People</span>
+            </div>
         </div>
         <!-- Ingredients -->
         <div id="recipeIngredients">
-            Ingredients Placeholder
+            <span class="md-title"><b>Ingredients</b></span><br>
+            <ul>
+                <li v-for="ingredient in ingredients">{{ingredient}}</li>
+            </ul>
         </div>
         <!-- Cookware -->
         <div id="recipeCookware">
-            Cookware Placeholder
+            <span class="md-title"><b>Cookware</b></span><br>
+            Nothing here!
         </div>
         <!-- Steps -->
         <div id="recipeSteps">
-            Steps Placeholder
+            <span class="md-title"><b>Steps!</b></span><br>
+            <div v-for="step, key in steps" class="recipeStepsStep">
+                <span class="md-subheading"><b>Step {{key}}</b></span><br>
+                <span class="md-body-1">{{step}}</span>
+            </div>
         </div>
     </div>
 </template>
 
 <!-- JS -->
 <script>
+  import MdInputContainer from "../../node_modules/vue-material/src/components/mdInputContainer/mdInputContainer";
+  import MdButton from "../../node_modules/vue-material/src/components/mdButton/mdButton.vue";
     export default {
+      components: {
+        MdButton,
+        MdInputContainer},
       name: 'recipePage',
       data() {
         return {
           //Temporary recipe data
           "submitter": "TxNVpLUsgAZc5yuoGul",
           "name": "Chicken Pot Pie",
-          "description": "A delicious chicken pot pie",
+          "description": "A delicious chicken pot pie!",
           "ingredients": {
-            "0": "chicken",
-            "1": "flour"
+            "0": "Chicken",
+            "1": "Flour"
           },
           "steps": {
-            "0": "Put chicken in a pie (any kind), and then put that pie in a pot, and put the pot in the oven.",
-            "1": "Cook for an hour at 500 degrees."
+            "0": "Somebody once told me the world is gonna roll me",
+            "1": "I ain't the sharpest tool in the shed",
+            "2": "She was looking kind of dumb with her finger and her thumb",
+            "3": "In the shape of an \"L\" on her forehead",
+            "4": "Well the years start coming and they don't stop coming",
+            "5": "Fed to the rules and I hit the ground running",
           },
           "prepTime": 20,
           "cookTime": 60,
           "servings": 5,
           "photos": {
-            "0": "gs://bucket/images/RxCOpBQsgWVc5yuoPPK.jpg"
+            "0": "https://i.imgur.com/YWbMCs0.png",
+            "1": "https://i.imgur.com/YWbMCs0.png",
+            "2": "https://i.imgur.com/YWbMCs0.png",
+            "3": "https://i.imgur.com/YWbMCs0.png",
+            "4": "https://i.imgur.com/YWbMCs0.png"
           },
-          "rating": 4.3
+          "rating": 4.3,
+          "reviews": [
+
+          ],
+          "madeIt": 45
         };
       }
     }
@@ -81,57 +134,110 @@
 
 <!-- CSS; scoped keyword added to restrict CSS to this component -->
 <style scoped>
-    #recipePage { /* Grid */
+    #summaryFlexButtons {
+        margin-top: 15px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    .summaryButtons {
+        flex: 1;
+        margin: 4px 0px;
+        border-radius:0px;
+    }
+    #recipePage {
         display: grid;
         grid-template-rows: repeat(3, minmax(50px ,auto));
         grid-template-columns: repeat(5, minmax(50px, auto));
         grid-gap: 5px;
-        background-color: #ffffff;
+        /*background-color: #ffffff;*/
+        padding-right: 20px;
+        padding-left: 20px;
+        padding-top: 30px;
     }
     #recipeSummary {
         grid-row: 1 / 2;
         grid-column: 1 / 4;
 
-        outline-color: blueviolet;
+        /*outline-color: blueviolet;
         outline-width: 2px;
-        outline-style: solid;
+        outline-style: solid;*/
+    }
+    #recipeSummaryTitle {
+        text-align: center;
     }
     #recipeImages {
         grid-row: 1 / 2;
         grid-column: 4 / 6;
+        margin-left: 10px;
+        /*outline-color: #32e24b;
+        outline-width: 2px;
+        outline-style: solid;*/
+    }
+    .recipePrimaryImage {
+        width:320px;
+    }
+    #recipeSecondaryImages {
+        display: flex;
+        flex-wrap: wrap;
+        max-width: 320px;
+    }
+    .recipeSecondaryImage {
+        flex: 1;
+        max-width: 77px;
+        align-self: center;
+    }
+    #recipeImageGallery {
+        flex: 1;
+        margin: 0 0;
+        padding: 0 0;
+        border-radius:0;
+        font-size: 24px;
+        max-width: 72px
+    }
+    #recipeDetails {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        grid-row: 2 / 3;
+        grid-column: 1 / 6;
+        margin-top: 10px;
+        margin-bottom: 10px;
 
-        outline-color: #32e24b;
+        outline-color: #ccdae2;
         outline-width: 2px;
         outline-style: solid;
     }
-    #recipeDetails {
-        grid-row: 2 / 3;
-        grid-column: 1 / 6;
-
-        outline-color: #e27219;
-        outline-width: 2px;
-        outline-style: solid;
+    .detailsFlexItem {
+        flex: 1;
+        margin: 4px 4px;
+        border-radius:0;
+        text-align: center;
     }
     #recipeIngredients {
         grid-row: 3 / 4;
         grid-column: 1 / 2;
 
-        outline-color: #3db8e2;
+        /*outline-color: #3db8e2;
         outline-width: 2px;
-        outline-style: solid;
+        outline-style: solid;*/
     }
     #recipeCookware {
 
-        outline-color: #d3e23b;
+        /*outline-color: #d3e23b;
         outline-width: 2px;
-        outline-style: solid;
+        outline-style: solid;*/
     }
     #recipeSteps {
         grid-row: 3 / 5;
         grid-column: 2 / 6;
+        margin-left: 10px;
 
-        outline-color: #32e233;
+        /*outline-color: #32e233;
         outline-width: 2px;
-        outline-style: solid;
+        outline-style: solid;*/
+    }
+    .recipeStepsStep {
+        margin-top: 10px;
     }
 </style>
