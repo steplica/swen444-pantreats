cce828fa64615bf29b4e327c8ad5965804f558ac
rsh3560 <rsh3560@g.rit.edu>
2017-11-04 20:34:42 -0400

    Made the review modal, changed small things in the report modal

--- src/partials/ReportModal.vue
+++ src/partials/ReportModal.vue
@@ -16,7 +16,7 @@
                 <v-divider></v-divider>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" dark @click.native="reportDialog = false">Cancel</v-btn>
+                    <v-btn color="primary" flat @click.native="reportDialog = false">Cancel</v-btn>
                     <v-btn color="primary" dark @click.native="reportDialog = false">Submit</v-btn>
                 </v-card-actions>
             </v-card>
---
+++ src/partials/ReviewModal.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-layout row justify-center>
+        <v-dialog v-model="reviewDialog" width="600px" padding="10px" persistent >
+            <v-btn color="primary" dark slot="activator">Open review Dialog</v-btn>
+            <v-card>
+                <v-card-title class="headline" font-weight: bold>Rate and Review</v-card-title>
+                <v-divider></v-divider>
+                <v-card-text>
+                    Your rating
+                    <md-rating-bar v-model="reviewRating" class="md-primary accent-tertiary"></md-rating-bar>
+                    <v-text-field v-model="reviewInfo" label="Your Review (Optional)" multi-line></v-text-field>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" flat @click.native="reviewDialog = false">Cancel</v-btn>
+                    <v-btn color="primary" dark @click.native="reviewDialog = false">Submit</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-layout>
+</template>
+
+<script>
+    import Firebase from 'firebase';
+    import db from '../database';
+    import router from 'vue-router';
+
+    export default {
+        name: 'reviewModal',
+        data () {
+            return {
+                reviewDialog: false,
+                reviewRating: undefined,
+                reviewInfo: undefined
+            };
+        },
+    }
+</script>
