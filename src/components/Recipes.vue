80431ae1e455ead2dc37462af21f41798fcbe61a
Matt Newell <developer.newell@gmail.com>
2017-11-19 13:35:48 -0500

    Search recipes (#8)
    * Add basic recipe page implementation
    
    * Recipes page now loads recipes from database
    
    * Add temporary button to refresh search results
    
    * Implement live result refresh

---
+++ src/components/Recipes.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="recipe-search-page">
+        <v-container fluid grid-list-lg>
+            <v-layout row wrap>
+                <v-flex xs3 v-for="recipe in recipes" :key="recipe.id">
+                    <recipe-card :name="recipe.name"
+                                 :src="recipe.photos[0]"
+                                 :description="recipe.description"
+                                 :rating="recipe.rating"
+                                 :id="recipe.id">
+                    </recipe-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    import Firebase from 'firebase';
+    import db from '../database';
+    import RecipeCard from '../partials/RecipeCard';
+    import $ from 'jquery/dist/jquery.min';
+
+    const recipesRef = db.ref('recipes/');
+
+    export default {
+      name: 'recipes',
+      components: {
+        RecipeCard
+      },
+      props: ['sharedPantry'],
+      data() {
+        return {
+          recipes: [] // TODO: Use VueFire
+        };
+      },
+      created: function() {
+        this.updateSearchResults();
+      },
+      watch: {
+        sharedPantry() {
+          this.updateSearchResults();
+        }
+      },
+      methods: {
+        updateSearchResults() {
+          // TODO: Replace with actual updates based on pantry ingredients
+          // For now, we just load the recipes that are in the database
+          this.recipes = [];
+          let recipes = this.recipes;
+
+          recipesRef.orderByChild('rating').on('child_added', (snapshot) => {
+            let recipe = snapshot.val();
+            recipe['id'] = snapshot.key; // TODO: Would it make sense to just store this in the database?
+
+            if (recipe.ingredients.every(ingredient => this.sharedPantry.includes(ingredient))) {
+              recipes.push(recipe);
+            }
+          });
+
+          this.recipes = recipes.reverse();
+        }
+      },
+    }
+</script>
